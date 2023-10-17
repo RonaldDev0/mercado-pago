3bac84d76ee016a08236a32cf312ecdb52f8b493
Ronald <ronaldzamoradev@gmail.com>
2023-10-16 22:16:03 -0500

    feat: avoid duplicate payments

--- src/app/api/process_payment/route.ts
+++ src/app/api/process_payment/route.ts
@@ -4,7 +4,7 @@ import { MercadoPagoConfig, Payment } from 'mercadopago'
 export async function POST (req: NextRequest) {
   const body = await req.json()
 
-  const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN!, options: { timeout: 5000, idempotencyKey: undefined } })
+  const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN!, options: { timeout: 5000, idempotencyKey: crypto.randomUUID() } })
   const payment = new Payment(client)
   const response = await payment.create({ body })
 
